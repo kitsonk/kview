3f53708dd536808e2bf88de5a3e64050eaf72dc6
hashrock <hashrock@users.noreply.github.com>
2023-11-12 02:50:20 +0900

    feat: add breadcrumbs to orgs, projects

--- routes/orgs/index.tsx
+++ routes/orgs/index.tsx
@@ -2,11 +2,15 @@ import { Head } from "$fresh/runtime.ts";
 import { AppFrame } from "$components/AppFrame.tsx";
 import { OrgList } from "$components/OrgList.tsx";
 import { getRootData } from "$utils/dash.ts";
+import { BreadcrumbItem } from "$components/Breadcrumbs.tsx";
 
 export default async function Home() {
   const data = await getRootData();
+  const breadcrumbs: BreadcrumbItem[] = [
+    { href: "/orgs", text: "Organizations" },
+  ];
   return (
-    <AppFrame>
+    <AppFrame breadcrumbs={breadcrumbs}>
       <Head>
         <title>Organizations &mdash; kview</title>
       </Head>
--- routes/projects/[name]/index.tsx
+++ routes/projects/[name]/index.tsx
@@ -2,19 +2,41 @@ import { Head } from "$fresh/runtime.ts";
 import { type RouteContext } from "$fresh/server.ts";
 import { AppFrame } from "$components/AppFrame.tsx";
 import { DbList } from "$components/DbList.tsx";
-import { getProjectDbs } from "$utils/dash.ts";
+import { getProjectDbs, getProjectDetails } from "$utils/dash.ts";
+import { BreadcrumbItem } from "$components/Breadcrumbs.tsx";
 
 export default async function OrganizationDetails(
   _req: Request,
   { params: { name } }: RouteContext,
 ) {
+  const projectDetail = await getProjectDetails(name);
+  const isUser = projectDetail.organization.name === null;
+
+  const breadcrumbs: BreadcrumbItem[] = isUser
+    ? [
+      { href: "/user", text: "User" },
+      { href: `/projects/${name}`, text: name },
+    ]
+    : [
+      { href: "/orgs", text: "Organizations" },
+      {
+        href: `/orgs/${projectDetail.organization.id}`,
+        text: projectDetail.organization.name ?? "",
+      },
+      { href: `/projects/${name}`, text: name },
+    ];
+
   const dbs = await getProjectDbs(name);
   return (
-    <AppFrame>
+    <AppFrame breadcrumbs={breadcrumbs}>
       <Head>
         <title>{name} &mdash; kview</title>
       </Head>
       <DbList project={name} dbs={dbs} />
     </AppFrame>
   );
+}
+
+function renderNotFound() {
+  throw new Error("Function not implemented.");
 }
