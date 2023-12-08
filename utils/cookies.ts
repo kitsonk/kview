3e768492967a1398bd389e428309117dc1d86454
Kitson Kelly <me@kitsonkelly.com>
2023-12-08 13:46:04 +1100

    chore: updated std

--- utils/cookies.ts
+++ utils/cookies.ts
@@ -1,5 +1,8 @@
 import { KeyStack } from "$std/crypto/keystack.ts";
-export { mergeHeaders, SecureCookieMap } from "$std/http/cookie_map.ts";
+export {
+  mergeHeaders,
+  SecureCookieMap,
+} from "$std/http/unstable_cookie_map.ts";
 
 export const ACCESS_TOKEN = "access_token";
 export const STORE_NAMES = "store_names";
