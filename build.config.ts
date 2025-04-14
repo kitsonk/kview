26c881315ffa1c7b622cbcf6eb7bff3f3431554d
Kitson Kelly <me@kitsonkelly.com>
2025-04-15 07:42:55 +1000

    chore: migrate to svg-minify plugin

--- build.config.ts
+++ build.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
-import minify from "./minify.plugin.ts";
+import svg_minify from "@kitsonk/svg-minify";
 import zipBuild from "./zip_build.plugin.ts";
 
 export default defineConfig({
-  plugins: [tailwind(), minify(), zipBuild()],
+  plugins: [tailwind(), svg_minify(), zipBuild()],
 });
